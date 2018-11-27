976a8ddc65f12a3f284fc5434a9fdb5e4549bac2
knine3 <kawishqayyum@gmail.com>
2018-11-27 06:14:24 +0500

    Add: 'Home' and 'Posts' templates
    Used Jinja2 to add:
      - For loop
      - If Conditional

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+    user = {'username': 'Knine3'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/posts')
+def posts():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful Evening in Karachi'
+        },
+        {
+            'author': {'username': 'Fazeel'},
+            'body': 'Clash Royale is so cool, yo!'
+        }
+    ]
+    return render_template('posts.html', title='Posts', posts=posts)
